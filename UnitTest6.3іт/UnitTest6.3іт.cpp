74e47e7f49cc45a1a637668463626bee52bc94a4
RitaPylypaka <ritapylypaka@gmail.com>
2023-11-01 17:04:50 +0200

    Add project files.

---
+++ UnitTest6.3іт/UnitTest6.3іт.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../пр6.3іт/пр6.3іт.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest63іт
+{
+	TEST_CLASS(UnitTest63іт)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int m = 8;
+			int n[m] = { 1,2,3,4,5,6,7,8 };
+			int a = Max(n, m);
+			Assert::AreEqual(8, a);
+		}
+	};
+}
---
+++ пр6.3іт/пр6.3іт.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+template <typename T>
+int Max(T a[], int size)
+{
+	int max = a[0];
+	for (int i = 1; i < size; i++) {
+		if (a[i] > max)
+			max = a[i];
+	}
+	return max;
+}
+void Create(int* a, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		a[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout <<  setw(4)  << a[i] << " ";
+	cout << endl;
+}
+int main()
+{
+	srand((unsigned)time(NULL)); 
+	const int n = 8;
+	int a[n];
+	int Low = -19;
+	int High = 40;
+	Create(a, n, Low, High);
+	Print(a, n);
+	cout << "max = " << Max(a, n) << endl;
+
+	return 0;}
